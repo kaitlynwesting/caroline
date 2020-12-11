998a10b0029a389dc4ce7547d6c3c20dea76f778
Kaitlyn <rainesconjecture@gmail.com>
2020-12-10 20:08:08 -0500

    Fix logging channel messages, add errortrapping for already muted members

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -18,21 +18,29 @@ class Moderation(commands.Cog):
     @commands.has_permissions(kick_members = True)
     async def mute(self, ctx, member: discord.Member):
         channel = discord.utils.get(ctx.guild.channels, name="logs") # god channel.id can be useful
-    
-        if get(ctx.guild.roles, name="Muted"):
-            muted = get(ctx.guild.roles, name="Muted")
-            await member.add_roles(muted)
+        
+        if get(ctx.guild.roles, name="Muted"): # if such a role exists in the server
 
+            muted = get(ctx.guild.roles, name="Muted")
             creator = get(ctx.guild.roles, name="Creator")
-            await member.remove_roles(creator)
-            await channel.send(f"{ctx.author.name} muted {member.mention}.") 
+
+            if muted in member.roles: # check if they already have muted role
+                await channel.send(f"{member.mention} is already muted.")
+            else:
+                await member.add_roles(muted)
+                await member.remove_roles(creator)
+
+                await member.send(f"You have been muted in the {ctx.guild.name} server.")
+                await channel.send(f"{ctx.author.name} muted {member.mention}.") 
         else:
             await ctx.guild.create_role(name="Muted", colour=discord.Colour(0x2C2F33)) # make new role if not existing
             muted = get(ctx.guild.roles, name="Muted")
-            await member.add_roles(muted)
-
             creator = get(ctx.guild.roles, name="Creator")
+            
+            await member.add_roles(muted)
             await member.remove_roles(creator)
+
+            await member.send(f"You have been muted in the {ctx.guild.name} server.")
             await channel.send(f"A new muted role was created. {ctx.author.name} has muted {member.mention}.") # finally!
 
     # TEXT UNMUTE COMMAND
@@ -41,57 +49,56 @@ class Moderation(commands.Cog):
     async def unmute(self, ctx, member: discord.Member):
         get(ctx.guild.roles, name="Muted")
         muted = get(ctx.guild.roles, name="Muted")
-        await member.remove_roles(muted)
-
         creator = get(ctx.guild.roles, name="Creator")
+        
+        await member.remove_roles(muted)
         await member.add_roles(creator)
 
         channel = discord.utils.get(ctx.guild.channels, name="logs") # god channel.id can be useful
+        await member.send(f"You have been unmuted in the {ctx.guild.name} server.")
         await channel.send(f"{ctx.author.name} unmuted {member.mention}.") 
     
     # KICK COMMAND
     @commands.command(name="kick")
     @commands.has_permissions(kick_members = True)
     async def kick(self, ctx, member: discord.Member, *, reason=None):
-        id = member.id
+        channel = discord.utils.get(ctx.guild.channels, name="logs") 
+
         if member == ctx.message.author:
             await ctx.channel.send("Are you daft? You can't kick yourself.")
             return
         if reason == None:
             reason = "Being an asshat"
-            realreason = "No rationale provided (defaulted to preset message)"
+            await channel.send(f"{ctx.author.name} kicked {member.mention} for the following reason: No rationale provided (defaulted to preset message).")
+        else:
+            print(channel)
+            await channel.send(f"{ctx.author.name} kicked {member.mention} for the following reason: {reason}")
 
         # dm the offending member the reason
         await member.send(f"You were kicked from {ctx.guild.name} for the following reason: {reason}.")
         await member.kick(reason=reason)
-        for channel in member.guild.channels:
-            if str(channel) == "logs":
-                print(f"{realreason}")
-                await channel.send(f"{ctx.author.name} kicked {member.mention} for the following cause: {realreason}") 
-                break
+        
     
     # BAN COMMAND
     @commands.command(name="ban")
     @commands.has_permissions(ban_members = True)
     async def ban(self, ctx, member: discord.Member, *, reason=None):
-        id = member.id
+        channel = discord.utils.get(ctx.guild.channels, name="logs")
+
         if member == ctx.message.author:
             await ctx.channel.send("Are you daft? You can't ban yourself.")
             return
         if reason == None:
             reason = "Being an asshat"
-            realreason = "No rationale provided (defaulted to preset message)"
-        
+            await channel.send(f"{ctx.author.name} banned {member.mention} for the following reason: No rationale provided (defaulted to preset message)")
+        else:
+            await channel.send(f"{ctx.author.name} banned {member.mention} for the following reason: {reason}")
+
         # dm the offending member the reason
         await member.send(f"You were banned from {ctx.guild.name} for the following reason: {reason}.")
         await member.ban(reason=reason)
-        for channel in member.guild.channels:
-            if str(channel) == "logs":
-                print(f"{realreason}")
-                await channel.send(f"{ctx.author.name} banned {member.mention} for the following cause: {realreason}") 
-                break
-
-
+         
+         
     # UNBAN COMMAND
     @commands.command(name="unban")
     @commands.has_permissions(ban_members = True)
