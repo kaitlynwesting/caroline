f180d2a43fd399938365e1a06487575d6631c138
Kaitlyn <rainesconjecture@gmail.com>
2020-12-11 22:00:54 -0500

    Add a token for Heroku

---
+++ settings.py
@@ -0,0 +1,10 @@
+import os
+
+prefix = "."
+token = "Nzg1Mjk4NTcyMDQ3NDE3Mzc0.X810SQ.CJ82UjzmAB9Ccts2CmfPmN_lE2w"
+nowplaying = "winter is coming | .help"
+
+# Base directory. Feel free to use it if you want.
+# BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+# git rm --cached settings.py
