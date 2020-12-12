de739be6daeacc8c13c2f1899847986d78478d1b
Kaitlyn <rainesconjecture@gmail.com>
2020-12-11 21:48:12 -0500

    Remove settings.py from tracking

--- settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-prefix = "."
-token = ""
-nowplaying = "winter is coming | .help"
-
-# Base directory. Feel free to use it if you want.
-# BASE_DIR = os.path.dirname(os.path.realpath(__file__))
