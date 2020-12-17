9b107090462fc78b5634732d69f481649cb5a6e6
Gad <haaruunibrow@gmail.com>
2020-12-16 21:35:47 -0600

    move to .env

--- settings.py
+++ settings.py
@@ -1,10 +1,8 @@
+from dotenv import load_dotenv
 import os
 
-prefix = "."
-token = "Nzg1Mjk4NTcyMDQ3NDE3Mzc0.X810SQ.CJ82UjzmAB9Ccts2CmfPmN_lE2w"
-nowplaying = "winter is coming | .help"
+load_dotenv()
 
-# Base directory. Feel free to use it if you want.
-# BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-
-# git rm --cached settings.py
+prefix = os.getenv("prefix")
+token = os.getenv("token")
+nowplaying = os.getenv("status")
