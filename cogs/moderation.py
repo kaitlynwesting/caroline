1193734a3970c5f825c22caed5064c2cffdc2c70
Kaitlyn <rainesconjecture@gmail.com>
2021-01-10 18:09:36 -0500

    Tidy up moderation cog structure

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -25,7 +25,7 @@ class Moderation(commands.Cog):
         elif member == ctx.message.author: # Stop muting yourself
             await ctx.channel.send("Are you daft? You can't mute yourself.")
 
-        elif member.top_role >= ctx.author.top_role and member.id != 669977303584866365:
+        elif member.top_role >= ctx.author.top_role and ctx.author.id != 669977303584866365:
             await ctx.send("How desperately you wish you could warn someone above or equal to your rank. But you can't. Boo, hoo.")
 
         else:
@@ -47,7 +47,7 @@ class Moderation(commands.Cog):
         elif member == ctx.message.author: # Stop muting yourself
             await ctx.channel.send("Are you daft? You can't mute yourself.")
 
-        elif member.top_role >= ctx.author.top_role: # Why would you try this
+        elif member.top_role >= ctx.author.top_role and ctx.author.id != 669977303584866365: # Why would you try this
             await ctx.send("How desperately you wish you could mute someone above or equal to your rank. But you can't. Boo, hoo.")
 
         else:
@@ -66,6 +66,7 @@ class Moderation(commands.Cog):
                 await member.send(f"You have been muted in the {ctx.guild.name} Discord.") # dm user
                 await channel.send(f"**[MODERATION]** {ctx.author.name} muted {member.mention}.") 
 
+
     # TEXT UNMUTE COMMAND
     @commands.command()
     @commands.has_permissions(kick_members = True)
@@ -78,77 +79,83 @@ class Moderation(commands.Cog):
             await member.remove_roles(muted)
             await member.add_roles(creator)
             
-            await ctx.send(f"Unfortunately, {member.mention}'s unmuted now.")
+            await ctx.send(f"That's done, {member.mention}'s unmuted now.")
             await member.send(f"You have been unmuted in the {ctx.guild.name} Discord.") # dm user
             await channel.send(f"**[MODERATION]** {ctx.author.name} unmuted {member.mention}.") 
 
         else:
             await ctx.send(f"{member.display_name} is not muted.")
     
+
     # KICK COMMAND
     @commands.command(name="kick")
     @commands.has_permissions(kick_members = True)
     async def kick(self, ctx, member: discord.Member, *, reason=None):
         channel = discord.utils.get(ctx.guild.channels, name="logs") 
         
-        if member.top_role < ctx.author.top_role:
+        if member.id == 785298572047417374:
+            await ctx.send("You're not very bright, are you?")
+
+        elif member == ctx.message.author:
+            await ctx.channel.send("Are you daft? You can't kick yourself.")
+
+        elif member.top_role >= ctx.author.top_role and ctx.author.id != 669977303584866365:
+            await ctx.send("How desperately you wish you could kick someone above or equal to your rank. But you can't. Boo, hoo.")
+
+        else:
             if reason == None:
                 reason = "Being an asshat."
                 await channel.send(f"{ctx.author.name} kicked {member.mention} for the following reason: No rationale provided (defaulted to preset message).")
+
             else: # successful kick.
                 print(channel)
                 await channel.send(f"{ctx.author.name} kicked {member.mention} for the following reason: {reason}")
 
-                # dm the offending member the reason
-                await member.send(f"You were kicked from {ctx.guild.name} Discord for the following reason: {reason}")
-                await member.kick(reason=reason)
-        elif member.id == 785298572047417374:
-            await ctx.send("You're not very bright, are you?")
-        elif member == ctx.message.author:
-            await ctx.channel.send("Are you daft? You can't kick yourself.")
-            return
-        else:
-            await ctx.send("How desperately you wish you could kick someone above or equal to your rank. But you can't. Boo, hoo.")
-            
+            await member.kick(reason=reason)
     
     # BAN COMMAND
     @commands.command(name="ban")
     @commands.has_permissions(ban_members = True)
     async def ban(self, ctx, member: discord.Member, *, reason=None):
-        channel = discord.utils.get(ctx.guild.channels, name="logs")
+        channel = discord.utils.get(ctx.guild.channels, name="logs") 
         
-        if member.top_role < ctx.author.top_role:
+        if member.id == 785298572047417374:
+            await ctx.send("You're not very bright, are you?")
+
+        elif member == ctx.message.author:
+            await ctx.channel.send("Are you daft? You can't ban yourself.")
+
+        elif member.top_role >= ctx.author.top_role and ctx.author.id != 669977303584866365:
+            await ctx.send("How desperately you wish you could ban someone above or equal to your rank. But you can't. Boo, hoo.")
+
+        else:
             if reason == None:
                 reason = "Being an asshat."
-                await channel.send(f"{ctx.author.name} banned {member.mention} for the following reason: No rationale provided (defaulted to preset message)")
-            else:
+                await channel.send(f"{ctx.author.name} banned {member.mention} for the following reason: No rationale provided (defaulted to preset message).")
+
+            else: 
+                print(channel)
                 await channel.send(f"{ctx.author.name} banned {member.mention} for the following reason: {reason}")
 
-            # dm the offending member the reason
-            await member.send(f"You were banned from {ctx.guild.name} Discord for the following reason: {reason}")
             await member.ban(reason=reason)
-        elif member.id == 785298572047417374:
-            await ctx.send("You're not very bright, are you?")
-        else:
-            await ctx.send("How desperately you wish you could ban someone above or equal to your rank. But you can't. Boo, hoo.")
          
+    
     # UNBAN COMMAND
     @commands.command(name="unban")
     @commands.has_permissions(ban_members = True)
     async def unban(self, ctx, *, member):
-        banned_users = await ctx.guild.bans()
-        member_name, member_discriminator = member.split("#")
+        channel = discord.utils.get(ctx.guild.channels, name="logs")
+        bannedUsers = await ctx.guild.bans()
+        memberName, memberDiscriminator = member.split("#")
         state = False
 
-        for ban_entry in banned_users:
-            user = ban_entry.user
+        for banEntry in bannedUsers:
+            user = banEntry.user
             
-
-            if (user.name, user.discriminator) == (member_name, member_discriminator):
+            if (user.name, user.discriminator) == (memberName, memberDiscriminator):
                 await ctx.guild.unban(user) #make the unban
 
-                channel = discord.utils.get(ctx.guild.channels, name="logs") # god channel.id can be useful
-                await channel.send(f"{ctx.author.name} unbanned {user.mention}.") # finally!
+                await channel.send(f"**[MODERATION]** {ctx.author.name} unbanned {user.mention}.") # finally!
                 state = True
             else:
                 pass
@@ -157,7 +164,6 @@ class Moderation(commands.Cog):
             await ctx.send("Are you sure they're on the banlist? Couldn't find anyone matching that name.")
         else:
             pass
-
 
     # TEMPBAN (WIP)
     """ @client.command()
