a57db2d89b08d91a2b6a73cc23380dc4334de513
Kaitlyn <rainesconjecture@gmail.com>
2021-01-12 18:38:11 -0500

    Add entire tempmute cog

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
+from datetime import datetime, timezone, timedelta
+import pytz
 
+import asyncio
 import traceback
 import sys
 import random
@@ -16,7 +19,7 @@ class Moderation(commands.Cog):
     # WARN COMMAND
     @commands.command()
     @commands.has_permissions(kick_members = True)
-    async def warn(self, ctx, member: discord.Member, reason): # reason is not None as warnings must be meaningful
+    async def warn(self, ctx, member: discord.Member, *, reason): # reason is not None as warnings must be meaningful
         channel = discord.utils.get(ctx.guild.channels, name="logs") 
 
         if member.id == 785298572047417374:
@@ -32,6 +35,7 @@ class Moderation(commands.Cog):
             await ctx.send(f"📨 Delivered **warning** to {member.mention} ({reason}).")
             await member.send(f"You have received a **warning** from {ctx.guild.name} Discord for the following: {reason}")
             await channel.send(f"**[MODERATION]** {ctx.author.name} issued a warning to {member.mention} for the following: {reason}")
+
 
     # TEXT MUTE COMMAND
     @commands.command()
@@ -71,13 +75,96 @@ class Moderation(commands.Cog):
                 await ctx.send(f"📨 Applied **mute** to {member.mention} indefinitely.")
                 await member.send(f"You have been **muted** indefinitely in {ctx.guild.name} Discord for the following: {reason}")
 
-    # TEXT UNMUTE COMMAND
-    @commands.command()
-    @commands.has_permissions(kick_members = True)
-    async def unmute(self, ctx, member: discord.Member):
+
+    # TEMPMUTE (WIP)
+    @commands.command(aliases=["tempm", "tm"])
+    async def tempmute(self, ctx, member: discord.Member, duration, *, reason=None):
+        channel = discord.utils.get(ctx.guild.channels, name="logs")
         muted = get(ctx.guild.roles, name="Muted")
         creator = get(ctx.guild.roles, name="Creator")
+        
+        timeConvert = {"s": 1, "m": 60, "h": 3600, "d": 86400}
+        timeStringSingular = {"s": "second", "m": "minute", "h": "hour", "d": "day"}
+        timeStringPlural = {"s": "seconds", "m": "minutes", "h": "hours", "d": "days"}
+        
+        def convert(time):
+            try:
+                return int(time[:-1]) * timeConvert[time[-1]] # in seconds
+            except:
+                return time # in seconds
+        
+        def timeToString(time):
+            if time[:1] == "1" and len(time) == 2:
+                try:
+                    return str(f"{time[:-1]} {timeStringSingular[time[-1]]}") # in seconds
+                except:
+                    await ctx.send("You okay?")
+            else:
+                try:
+                    return str(f"{time[:-1]} {timeStringPlural[time[-1]]}") # in seconds
+                except:
+                    await ctx.send("You okay?")
+        
+        sleepTime = convert(duration) # convert everything to seconds!
+        displayTime = timeToString(str(duration))
+        
+        nowTime = datetime.now(tz=timezone.utc)
+        muteTime = timedelta(seconds=sleepTime)
+        finishTime = nowTime + muteTime
+        
+        muteTime = str(muteTime)
+        finishTime = str(finishTime)
+        finishTime = finishTime[:-13]
+
+        if member.id == 785298572047417374: # Lydia id lol
+            await ctx.send("You're not very bright, are you?")
+
+        elif member == ctx.message.author: # Stop muting yourself
+            await ctx.channel.send("Are you daft? You can't mute yourself.")
+
+        elif member.top_role >= ctx.author.top_role and ctx.author.id != 669977303584866365: # Why would you try this
+            await ctx.send("How desperately you wish you could mute someone above or equal to your rank. But you can't. Boo, hoo.")
+
+        else:
+            if muted in member.roles: # Do you already have muted role
+                await ctx.send(f"{member.mention} is already muted.")
+            else:
+                for role in member.roles:
+                    try:
+                        await member.remove_roles(role)
+                    except:
+                        pass 
+                
+                await member.add_roles(muted)
+                
+                if reason == None:
+                    reason = "Being an asshat."
+                    await channel.send(f"**[MODERATION]** {ctx.author.name} **tempmuted** {member.mention} for the following reason: No rationale provided (defaulted to preset message). The tempmute will last until {finishTime} ({displayTime}) UTC.")
+
+                else: 
+                    await channel.send(f"**[MODERATION]** {ctx.author.name} **tempmuted** {member.mention} for the following reason: {reason}. The tempmute will last until {finishTime} ({displayTime}) UTC.")
+                
+                await ctx.send(f"📨 Applying **tempmute** to {member.mention} until {finishTime} ({displayTime}) UTC.")
+                await member.send(f"You have been **tempmuted** in {ctx.guild.name} Discord for the following: {reason}. You will be unmuted automtically at {finishTime} UTC.")
+
+                await asyncio.sleep(sleepTime) # wait and snooze
+
+                if muted in member.roles: 
+                    await member.remove_roles(muted)
+                    await member.add_roles(creator)
+                    
+                    await member.send(f"You have been automatically **unmuted** in {ctx.guild.name} Discord.") # dm user
+                    await channel.send(f"**[MODERATION]** 📨 {member.mention} has been automatically **unmuted** now.") 
+        
+
+
+    # TEXT UNMUTE COMMAND
+    @commands.command(aliases=["um"])
+    @commands.has_permissions(kick_members = True)
+    async def unmute(self, ctx, member: discord.Member):
         channel = discord.utils.get(ctx.guild.channels, name="logs") 
+        muted = get(ctx.guild.roles, name="Muted")
+        creator = get(ctx.guild.roles, name="Creator")
 
         if muted in member.roles: 
             await member.remove_roles(muted)
@@ -172,15 +259,7 @@ class Moderation(commands.Cog):
         else:
             pass
 
-    # TEMPBAN (WIP)
-    """ @client.command()
-    async def tempban(ctx, member: discord.Member, duration:int, *, reason=None):
         
-        
-        await member.send(f"You were banned from {ctx.guild.name} for the following reason: {reason}.")
-        await member.ban(reason=reason)
-        print(reason)
-        await ctx.send(f"{ctx.author.name} tempbanned {member.mention} for a duration of {duration} minutes, for the following cause:  {realreason}")   """
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
