c6b1db4a7ee01dbd3335d880f1eb80ef9e8f3a03
Kaitlyn <rainesconjecture@gmail.com>
2021-03-10 20:50:13 -0500

    Chonk the holy chonk

--- cogs/filters/mass.py
+++ cogs/filters/mass.py
@@ -1,55 +1,42 @@
-import asyncio
-from datetime import datetime, timezone, timedelta
-
 import discord
 from discord.ext import commands
 from discord.utils import get
 
 from ..utils import infraction
-import os
-import pytz
-import random
-import requests
-import sys
-import string
-import time
-import traceback
+from ..utils import filter_bypass
 
-# COG TO FILTER CHAT FOR MASS MENTIONS
+# A mass mentions filter which aims to detect mass pings and silence abusers
+
 class Mass(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
-    # ANTI-ADVERTISING FILTER
     @commands.Cog.listener()
     async def on_message(self, message):
-        # Detect advertisements only in the server!
-        if message.guild is not None:
-            mod = get(message.guild.roles, name = "Moderator")
-            admin = get(message.guild.roles, name = "Admin")
+        
+        if "@everyone" in message.content or "@here" in message.content:
 
-            omitted_roles = [mod, admin]
+            can_bypass = await filter_bypass.bypass_check(message) # Boolean - are they exempt from filter?
+            print(message.author, 'CAN BYPASS IS...', can_bypass)
 
-            punishment_time = 10
+            if can_bypass == False:
 
-            with open('cogs/texts/infraction.txt', 'r') as f:
-                rules = f.readlines()
+                punishment_time = 10 # Time, in minutes, of automute time
 
-            rule1 = (rules[2])[0:-1]
-            rule2 = (rules[3])[0:-1]
-            rule3 = ((rules[14])[0:-1]).replace("punishment", str(punishment_time))
-            
-            infraction_description = f"""__Violation of {rule1}: {rule2}__ 
-Apparently, you deemed yourself important enough to disturb others.
-{rule3}. {rules[13]} """
-            
-            # The action parts.
-            if not any(r in message.author.roles for r in omitted_roles):
-                if "@everyone" in message.content or "@here" in message.content:
-                    await infraction.infraction_notification(message, "mass mentions", punishment_time)
-                    await infraction.infraction_auto_embed(message, "mass mentions", infraction_description)
-                    await infraction.infraction_tempmute(message, punishment_time)
+                with open('cogs/texts/infraction.txt', 'r') as f:
+                    rules = f.readlines()
+
+                rule1 = (rules[2])[0:-1]
+                rule2 = (rules[3])[0:-1]
+                rule3 = ((rules[14])[0:-1]).replace("punishment", str(punishment_time))
+ 
+                infraction_description = ("Apparently, you deemed yourself important enough to disturb ~400 people. "
+                                        f"{rule3}. {rules[13]}")
+        
+                await infraction.infraction_notification(message, "mass mentions", punishment_time)
+                await infraction.infraction_auto_embed(message, "mass mentions", infraction_description)
+                await infraction.infraction_tempmute(message, punishment_time)
 
 def setup(bot):
     bot.add_cog(Mass(bot))
