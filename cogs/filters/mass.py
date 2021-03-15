9a25700bf6b18a9696d4b1ba554d284837b97192
Kaitlyn <rainesconjecture@gmail.com>
2021-03-15 12:24:38 -0400

    Tweak minor PEP8 errors

--- cogs/filters/mass.py
+++ cogs/filters/mass.py
@@ -1,11 +1,11 @@
-import discord
 from discord.ext import commands
 from discord.utils import get
 
 from ..utils import infraction
 from ..utils import filter_bypass
 
-# A mass mentions filter which aims to detect mass pings and silence abusers
+# A mass mentions filter which aims to detect mass pings and shut up weirdos
+
 
 class Mass(commands.Cog):
 
@@ -22,7 +22,7 @@ class Mass(commands.Cog):
 
             if can_bypass == False:
 
-                punishment_time = 10 # Time, in minutes, of automute time
+                punishment_time = 10  # Time, in minutes, of automute time
 
                 with open('cogs/texts/infraction.txt', 'r') as f:
                     rules = f.readlines()
@@ -32,11 +32,12 @@ class Mass(commands.Cog):
                 rule3 = ((rules[14])[0:-1]).replace("punishment", str(punishment_time))
  
                 infraction_description = ("Apparently, you deemed yourself important enough to disturb ~400 people. "
-                                        f"{rule3}. {rules[13]}")
+                                          f"{rule3}. {rules[13]}")
         
                 await infraction.infraction_notification(message, "mass mentions", punishment_time)
                 await infraction.infraction_auto_embed(message, "mass mentions", infraction_description)
                 await infraction.infraction_tempmute(message, punishment_time)
+
 
 def setup(bot):
     bot.add_cog(Mass(bot))
