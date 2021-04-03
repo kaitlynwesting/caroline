fc75fd3b98146da01ae1df365e58df78e0d25659
Kaitlyn <rainesconjecture@gmail.com>
2021-04-03 13:11:20 -0400

    Update requirements.txt for library vulnerabilities

--- bot.py
+++ bot.py
@@ -2,9 +2,6 @@ import discord
 from discord.ext import commands
 
 import os
-
-from discord.ext.commands import bot
-
 import settings
 
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
@@ -18,11 +15,12 @@ async def on_ready():
 
     print("I am ready!")
 
-    # set the now playing status
-
     print("Setting now playing game...", flush=True)
+
     await client.change_presence(
-        activity=discord.Activity(type=discord.ActivityType.listening, name=settings.nowplaying))
+        activity=discord.Activity(type=discord.ActivityType.listening, name=settings.nowplaying)
+    )
+
     print("-----")
 
     # The below is a loader of cogs.
@@ -37,36 +35,6 @@ async def on_ready():
                     except Exception as e:
                         print(f"Could not load {file} due to {e}")
 
-        # try:
-        #     print(os.listdir(f'./cogs/{item}'))
-        # except NotADirectoryError as e:
-        #     print("Oops, couldn't do that because of", e)
+# heroku ps -a robolydia #(twd) tie city 206
 
-    print('over')
-#     path = ["./cogs", "./cogs/filters"]
-#     loading_path = ""
-#     for folder in path:
-#         for filename in os.listdir(folder):
-#             if folder == "./cogs":
-#                 loading_path = f"cogs.{filename[:-3]}"
-#             elif folder == "./cogs/filters":
-#                 loading_path = f"cogs.filters.{filename[:-3]}"
-#
-#             if filename.endswith(".py"):
-#                 try:
-#                     client.load_extension(loading_path)
-#                     print(f"Loaded: {filename}")
-#                 except Exception as e:
-#                     print(e)
-#                     print(f"Failed to load {filename}")
-#         print("-----")
-#
-#     # vanity print messages
-#     print("Logged in as:", client.user.name)
-#     print("My id is:", client.user.id)
-#     print("My prefix is:", settings.prefix)
-#     print('-----')
-#     # heroku ps -a robolydia #(twd) tie city 206
-#
-#
 client.run(settings.token)
