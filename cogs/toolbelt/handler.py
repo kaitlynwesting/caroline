c7efa61e1dc6d04ef54dc8097d7d46219417288d
Kaitlyn <rainesconjecture@gmail.com>
2021-04-11 13:15:36 -0400

    Fix not adding mute roles when muted, default infraction mute time is 7d

--- cogs/toolbelt/handler.py
+++ cogs/toolbelt/handler.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 
+import traceback
+
 
 class Handler(commands.Cog):
 
@@ -11,29 +13,38 @@ class Handler(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
 
-        # INSUFFICIENT PARAMETERS
+        # Insufficient arguments.
         if isinstance(error, commands.MissingRequiredArgument):
-            mra = ["Think you're missing an argument or " + num + f", {ctx.message.author.display_name}.",
-                   "Insufficient arguments.",
-                   f"{ctx.message.author.display_name}, mate, you need more parameters."]
-            await ctx.send(random.choice(mra))
+            await ctx.send(
+                f"Oops, {ctx.author.name}, looks like you missed the following arguments: \n"
+                f"`{error.param}`"
+            )
+
+            print(ctx.command.short_doc)  # YES
+            # await ctx.send_help(ctx.command)
 
         # INSUFFICIENT PERMISSIONS
-        if isinstance(error, commands.MissingPermissions):
-            # await ctx.send("You simply have less value.")
-            # await ctx.send(error.missing_perms)
+        elif isinstance(error, commands.MissingPermissions):
             await ctx.send("You can't. Why?", file=discord.File("./media/image1.jpg"))
 
         # CAN'T FIND COMMAND
-        if isinstance(error, commands.CommandNotFound):
+        elif isinstance(error, commands.CommandNotFound):
 
-            if ctx.message.content.startswith("!d"):
-                pass
-            else:
+            if not ctx.message.content.startswith("!d"):
+
                 await ctx.send("Couldn't find that command, sorry.")
 
+        # CAN'T FIND PERSON
+        elif isinstance(error, commands.MemberNotFound):
+
+            await ctx.send(
+                "Couldn't find anyone, sorry. \n"
+                "To successfully convert someone to a `discord.Member` object, "
+                "use their ID, mention, or name."
+            )
+
         # ON COOLDOWN
-        if isinstance(error, commands.CommandOnCooldown):
+        elif isinstance(error, commands.CommandOnCooldown):
             mod = get(ctx.guild.roles, name="Moderator")
 
             if mod in ctx.author.roles:
@@ -46,8 +57,17 @@ class Handler(commands.Cog):
                 await ctx.send("You're on cooldown.")
 
         # BAD ARGUMENTS
-        if isinstance(error, commands.ArgumentParsingError):
+        elif isinstance(error, commands.ArgumentParsingError):
             await ctx.send("Bleh, smelled a bad argument.")
+
+        else:
+            await ctx.send(
+                f"Something just borked. Yup, it's an uncaught exception.\n"
+                f"You should probably scream at Kat now so she can fix me. (I kid, please don't shriek) \n\n"
+                f"**```{traceback.format_exception(type(error), error, error.__traceback__)}```**\n"
+                f"If you're the first person to have seen this type of error, {ctx.author.name}, congratulations! "
+                f"Uncaught exceptions are rare, and you deserve to be rewarded for your contribution to bug hunting."
+            )
 
 
 def setup(bot):
