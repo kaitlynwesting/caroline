c50e03d51f91003e86da316a9f90e92aa96e3446
Kaitlyn <rainesconjecture@gmail.com>
2021-04-22 10:26:06 -0400

    Fix unsubscribe command

--- cogs/toolbelt/subscription.py
+++ cogs/toolbelt/subscription.py
@@ -26,7 +26,7 @@ class Subscription(commands.Cog):
                 else:
                     await ctx.author.add_roles(announce)
                     await ctx.send(
-                        f"{ctx.author.display_name}, you've subscribed to our announcements updates!")  # tag actual channel here in real server
+                        f"{ctx.author.display_name}, you've subscribed to our announcements updates!")
 
             else:
                 await ctx.send("Hmm, we don't offer that subscription yet. Perhaps you meant something else?")
@@ -40,19 +40,10 @@ class Subscription(commands.Cog):
     async def unsubscribe(self, ctx, message):
         message = message.lower()
 
-        if ctx.channel.name == "bot-commands":
-            if (message == "totd"):
-                totd = get(ctx.guild.roles, name="Tip of the Day")
+        if ctx.channel.id == constants.bot_commands:
 
-                if totd in ctx.author.roles:  # check if they already have role
-                    await ctx.author.remove_roles(totd)
-                    await ctx.send(f"{ctx.author.display_name}, you've successfully unsubscribed from our daily tips!")
-                else:
-                    await ctx.send(
-                        f"You're not a subscriber. What is there to unsubscribe from?")  # tag actual channel here in real server
-
-            elif (message == "announcements"):
-                announce = get(ctx.guild.roles, name="Announcements")
+            if message == "announcements":
+                announce = ctx.guild.get_role(constants.announcements_sub)
 
                 if announce in ctx.author.roles:  # check if they already have role
                     await ctx.author.remove_roles(announce)
@@ -64,9 +55,9 @@ class Subscription(commands.Cog):
                 await ctx.send("That's not a valid subscription.")
 
         else:
-            channel = get(ctx.guild.channels, name="bot-commands")
+
             await ctx.send(
-                f"Please unsubscribe in the proper channel - that is, <#{channel.id}>.")  # UPDATE FOR REAL PS SERVER
+                f"Please unsubscribe in the proper channel - that is, <#{constants.bot_commands}>.")
 
 
 def setup(bot):
