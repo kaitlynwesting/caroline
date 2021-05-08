e437a5d19abd80295fe92696bddc1fa140b939c3
Kaitlyn <rainesconjecture@gmail.com>
2021-05-08 13:15:26 -0400

    Truncate error length

--- cogs/toolbelt/handler.py
+++ cogs/toolbelt/handler.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
-
+from utils import constants
 import traceback
 
 
@@ -61,15 +61,14 @@ class Handler(commands.Cog):
             await ctx.send("Bleh, smelled a bad argument.")
 
         else:
-            error_message = traceback.format_exception(type(error), error, error.__traceback__)
-
-            await ctx.send(
-                f"Something just borked. Yup, it's an uncaught exception.\n"
-                f"You should probably scream at Kat now so she can fix me. (I kid, please don't shriek) \n\n"
-                f"```{''.join([str(x) for x in error_message])}```"
-                f"If you're the first person to have seen this type of error, {ctx.author.name}, congratulations! "
-                f"Uncaught exceptions are rare, and you deserve to be rewarded for your contribution to bug hunting."
-            )
+            await ctx.send(error[:1000])
+            # error_message = traceback.format_exception(type(error), error, error.__traceback__)
+            #
+            # await ctx.send(
+            #     f"Something just borked. Yup, it's an uncaught exception.\n"
+            #     f"Pinging <@{constants.kat_id}> so she can fix this as soon as possible. \n\n"
+            #     f"```{''.join([str(x) for x in error_message])}```"
+            # )
 
 
 def setup(bot):
