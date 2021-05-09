882fd8ecb97e6a01f5fa5c5296c2a28eb5f35fba
Kaitlyn <rainesconjecture@gmail.com>
2021-05-09 11:51:41 -0400

    Fix purge command

--- cogs/moderation/cleaner.py
+++ cogs/moderation/cleaner.py
@@ -1,8 +1,4 @@
 import discord
-import traceback
-import sys
-import random
-import datetime
 from discord.ext import commands
 
 
@@ -11,29 +7,29 @@ class Cleaner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    @commands.has_permissions(kick_members=True)
-    async def clean(self, ctx, amount: int):
-        await ctx.channel.purge(limit=amount + 1, check=None, bulk=True)
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
-    async def clear(self, ctx, num: int, target):
+    async def purge(self, ctx, num: int, target: discord.User):
         if num > 500 or num < 0:
             return await ctx.send("Invalid amount. Maximum is 500.")
 
-        def msgcheck(msg):
+        def checker(m):
             if target is not None:
-                return msg.author.id == target
+                return m.author.id == target.id
             return True
 
-        deleted = await ctx.channel.purge(limit=num, check=msgcheck)
-        await ctx.send(f':thumbsup: Deleted **{len(deleted)}/{num}** possible messages for you.', delete_after=10)
+        deleted = await ctx.channel.purge(limit=num, check=checker)
+        await ctx.channel.send(f'👌 Deleted {len(deleted)} message(s).')
+
+        # deleted = await ctx.channel.purge(limit=num, check=msgcheck)
+        # await ctx.send(f':thumbsup: Deleted **{len(deleted)}/{num}** possible messages for you.')
 
     """ @commands.command()
     @commands.has_permissions(kick_members = True)
     async def wipe(self, ctx, amount=2, before=datetime.datetime.now(), after = datetime.datetime(2020, 5, 17)):
-        await ctx.channel.purge(limit=amount + 1, check=None, before=None, after=None, around=None, oldest_first=False, bulk=True) """
+        await ctx.channel.purge(limit=amount + 1, check=None, before=None, after=None, around=None, oldest_first=False, bulk=True) 
+    """
 
 
 def setup(bot):
