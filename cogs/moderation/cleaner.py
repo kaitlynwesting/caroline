9fe7c7e7b1caf0502c3818894b977b675ff74a08
Kaitlyn <rainesconjecture@gmail.com>
2021-05-22 12:15:04 -0400

    Update cleaner.py

--- cogs/moderation/cleaner.py
+++ cogs/moderation/cleaner.py
@@ -7,44 +7,59 @@ class Cleaner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    @commands.has_permissions(manage_messages=True)
-    async def purge(self, ctx, limit: int, target_member: discord.User = None):
+    @commands.guild_only()
+    @commands.group(invoke_without_command=True, aliases=["clean", "scrub", "delete", "deletus"])
+    @commands.has_permissions(kick_members=True)
+    async def purge(self, ctx, limit: int = 2):
         """
-        Clears messages.
-        Note: limit refers to HOW MANY messages the bot should scan. It does not refer to how many messages to delete
-        from a person.
-        Note: if target_member is not specified, the bot will delete messages from a channel regardless of the author.
-        Example: !purge 100 669977303584866365 # Scans last 100 messages in a channel, and delete any messages from
-        user with ID 669977303584866365
+        Purges the most recent number of messages from a channel. No filters are applied.
+        Note: limit will default to 1, if limit is not specified.
+        Example: !purge 10
 
         :param ctx:
-        :param limit: int
-        :param target_member: discord.Member
+        :param limit: int = 1
         :return:
         """
+
+        try:
+            deleted_amount = await ctx.channel.purge(limit=limit)
+            notification = await ctx.channel.send(f'👌 Deleted {len(deleted_amount)} message(s).')
+            await notification.delete(delay=5)
+        except Exception as e:
+            notification = await ctx.channel.send(f'👌 Deleted messages. Caught an exception - {e} - for you.')
+            await notification.delete(delay=5)
+
+    @commands.guild_only()
+    @purge.command(invoke_without_command=True, aliases=["clean", "scrub", "delete", "deletus"])
+    @commands.has_permissions(kick_members=True)
+    async def user(self, ctx, purged_user: discord.User, limit: int = None):
+        """
+                Purges messages from a specific user.
+                Note: limit refers to HOW MANY messages the bot should scan. It does not refer to how many messages to delete
+                from a person.
+                Note: if target_member is not specified, the bot will delete messages from a channel regardless of the author.
+                Example: !purge 100 669977303584866365 # Scans last 100 messages in a channel, and delete any messages from
+                user with ID 669977303584866365
+
+                :param ctx:
+                :param purged_user: discord.Member
+                :param limit: int
+                :return:
+                """
 
         if limit > 500 or limit < 0:
             return await ctx.send("Invalid amount. Maximum is 500.")
 
         def checker(m):
-            if target_member is not None:
-                return m.author.id == target_member.id
-            return True
+            return m.author.id == purged_user.id
 
         try:
-            deleted = await ctx.channel.purge(limit=limit, check=checker)
-            await ctx.channel.send(f'👌 Deleted {len(deleted)} message(s).')
-        except discord.errors.NotFound as e:
-            await ctx.channel.send(f'👌 Deleted messages. Caught {e} for you.')
-
-
-
-    """ @commands.command()
-    @commands.has_permissions(kick_members = True)
-    async def wipe(self, ctx, amount=2, before=datetime.datetime.now(), after = datetime.datetime(2020, 5, 17)):
-        await ctx.channel.purge(limit=amount + 1, check=None, before=None, after=None, around=None, oldest_first=False, bulk=True) 
-    """
+            deleted_amount = await ctx.channel.purge(limit=limit, check=checker)
+            notification = await ctx.channel.send(f'👌 Deleted {len(deleted_amount)} message(s).')
+            await notification.delete(delay=5)
+        except Exception as e:
+            notification = await ctx.channel.send(f'👌 Deleted messages. Caught an exception - {e} - for you.')
+            await notification.delete(delay=5)
 
 
 def setup(bot):
