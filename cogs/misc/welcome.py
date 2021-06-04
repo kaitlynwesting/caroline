700b3b7949c26c62872186b6db983043d5e3b23e
Kaitlyn <rainesconjecture@gmail.com>
2021-06-04 19:55:28 -0400

    Add partnership message to welcome message

--- cogs/misc/welcome.py
+++ cogs/misc/welcome.py
@@ -1,13 +1,11 @@
 import discord
 from discord.ext import commands
-from discord.utils import get
 
 from utils import constants
 
 
 class Welcome(commands.Cog):
 
-    # Constructor
     def __init__(self, bot):
         self.bot = bot
 
@@ -19,6 +17,7 @@ class Welcome(commands.Cog):
             print(member)
 
             user_message = (
+                f"**Now partnered with the [r/Photoshop](https://www.reddit.com/r/photoshop/) subreddit! ^_^** \n\n"
                 f"Thank you for accepting our rules! "
                 f"For reference, you can find them again in <#{constants.rules}>.\n\n"
                 f"**Here's a fast rundown on what to do now, in the server:**\n",
@@ -27,7 +26,7 @@ class Welcome(commands.Cog):
                 f"• Technical help with Photoshop is exchanged in our help channels: <#{constants.alpha}> + ",
                 f"<#{constants.beta}>. We'll help you to the best of our ability.\n\n",
                 f"• To receive advice and suggestions for your ongoing works in progress (WIPs), head over to",
-                f"<#{constants.critique}> and post your art.\n\n",
+                f"<#{constants.critique}> and post your work, or give advice to others.\n\n",
                 f"• Improve your skills and make fantastic art by participating in the most recent event in our",
                 f"<#{constants.events}> channel, which changes weekly :)\n\n"
                 f"That's it for now! We wish you an enjoyable and excellent stay. See you inside!"
