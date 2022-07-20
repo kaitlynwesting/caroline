c8aaacdfe9da71f81dbe352dd63b1ba15cf2c901
Kaitlyn <kaitlynwesting@gmail.com>
2022-07-20 13:15:45 -0400

    Add persistent view loading for dropdowns on restart

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import aiosqlite
 import discord
+import settings
 import traceback
 from discord.ext import commands
-import settings
-from cogs.dropdowns import DropdownView
-from cogs.help import MyHelp
+from cogs.utils.views import DropdownView
+from cogs.help import CaroHelp
 from cogs.utils.views import Context
 
 intents = discord.Intents(
@@ -17,14 +17,15 @@ class Caroline(commands.Bot):
         super().__init__(
             command_prefix=commands.when_mentioned_or(settings.prefix),
             intents=intents,
-            help_command=MyHelp(),
+            help_command=CaroHelp(),
             activity=discord.Activity(type=discord.ActivityType.listening, name=settings.activity),
             status=discord.Status.idle
         )
+        self.db = None
+        self.id = 785298572047417374
         self.persistent_views_added = False
         self.initial_extensions = [
             "cogs.admin",
-            "cogs.misc",
             "cogs.moderation",
             "cogs.photoshop",
             "cogs.meta",
@@ -34,13 +35,10 @@ class Caroline(commands.Bot):
     async def get_context(self, message, *, cls=Context):
         return await super().get_context(message, cls=cls)
 
-    async def on_connect(self):
-        bot.db = await aiosqlite.connect("data/guild.db")
+    # async def on_connect(self):
+    #     self.db = await aiosqlite.connect("data/guild.db")
 
     async def on_ready(self):
-        if not self.persistent_views_added:
-            self.add_view(DropdownView())
-            self.persistent_views_added = True
 
         print(
             f"-----\n"
@@ -51,6 +49,8 @@ class Caroline(commands.Bot):
         )
 
     async def setup_hook(self):
+        self.db = await aiosqlite.connect("data/guild.db")
+
         print(f"Attempting to load {len(self.initial_extensions)} cogs...\n-----")
 
         for extension in self.initial_extensions:
@@ -62,6 +62,23 @@ class Caroline(commands.Bot):
                     f"Failed to load extension '{extension}'\n"
                     f"{traceback.format_exc()}"
                 )
+
+        if not self.persistent_views_added:
+            rows = await(
+                await self.db.execute("""SELECT * FROM dropdowns""")).fetchall()
+
+            groups = {}
+            for dropdown in rows:
+                groups.setdefault(dropdown[4], []).append(dropdown)
+
+            dropdowns_list = list(groups.values())
+
+            for num, l in enumerate(dropdowns_list):
+                self.add_view(view=DropdownView(dropdowns_list[num], '', 0, 0))
+
+            self.persistent_views_added = True
+
+            print("Dropdowns loaded.")
 
 
 bot = Caroline()
